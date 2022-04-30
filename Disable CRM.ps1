48dce8f26f0aa32d643adb2de0f217b2720fafbd
heytots <103478364+heytots@users.noreply.github.com>
2022-04-30 00:48:09 -0300

    Update Disable CRM.ps1

--- Disable CRM.ps1
+++ Disable CRM.ps1
@@ -1,1 +1,1 @@
-﻿Set-ItemProperty -Path "HKEY_CURRENT_USER\Software\Microsoft\Office\Outlook\Addins\Integration.CRM.Outlook" -name "LoadBehavior" -Value 2
+﻿Set-ItemProperty -Path 'HKEY_CURRENT_USER\Software\Microsoft\Office\Outlook\Addins\Integration.CRM.Outlook' -name "LoadBehavior" -Value 2
